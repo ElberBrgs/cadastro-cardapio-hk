416335bd2f82f8db14987b5b69b80b4bfb435577
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2025-10-25 02:23:02 -0300

    Update List

--- src/main/java/com/example/cadastro_cardapio_hk/controller/PratoController.java
+++ src/main/java/com/example/cadastro_cardapio_hk/controller/PratoController.java
@@ -20,7 +20,7 @@ public class PratoController {
         this.pratoService = pratoService;
     }
 
-    @GetMapping("/pratos")
+    @GetMapping
     public List<Prato> listarTodos() {
         return pratoService.listarTodos();
     }
@@ -33,4 +33,5 @@ public class PratoController {
                 .body(Map.of("mensagem", "Prato cadastrado com sucesso."));
     }
 }
+
 
